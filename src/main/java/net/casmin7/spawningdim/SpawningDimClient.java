6a34505950cfa7adf5050da8a2763abee3d13b98
Casmin7 <cosmin.ciurevici2017@gmail.com>
2023-04-01 22:01:35 +0300

    Initial commit

---
+++ src/main/java/net/casmin7/spawningdim/SpawningDimClient.java
@@ -0,0 +1,10 @@
+package net.casmin7.spawningdim;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpawningDimClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
